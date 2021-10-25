d439f3feec4008bcb7b66af4e898e78ac5a4d5db
joseph emmanuel kayode (iemarjay) <emarjay921@gmail.com>
2021-10-25 10:18:49 +0100

    fixed title

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -6,14 +6,14 @@ export const Layout = ({ page, title, description, children, metaImage }) => {
   return (
     <>
       <Head>
-        <title>{`${title ?? "Welcome"} - Piggy.FI Africa`}</title>
+        <title>{`${title ?? "Welcome"} - PiggyFI Africa`}</title>
         <meta
           name="description"
-          content={description ? description : "Piggy.FI a smart way to save"}
+          content={description ? description : "PiggyFI a smart way to save"}
         />
         <meta
           property="og:title"
-          content={`${title ?? "Welcome"} - Qlip NFT Academy`}
+          content={`${title ?? "Welcome"} - PiggyFI Africa`}
         />
         <meta
           property="og:description"
@@ -73,10 +73,6 @@ export const Layout = ({ page, title, description, children, metaImage }) => {
         <link rel="manifest" href="/manifest/manifest.webmanifest" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,100;1,200;1,300;1,400;1,500;1,600&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <Menu />
       {children}
